03c0f61fc924a0b6f0ea1ef490e9f75ec5e3cb1b
tligodsp <43437080+tligodsp@users.noreply.github.com>
2019-11-19 00:50:31 +0700

    Update daily goal

--- src/components/Learn/Learn.jsx
+++ src/components/Learn/Learn.jsx
@@ -8,9 +8,23 @@ const Learn = (props) => {
       <div className={classes['may-cai-ben-phai-container']}>
         <div className={classes['daily-goal-container']}>
           <h2 className={classes['daily-goal-title']}>
-            <span style={{ display: "table-cell" }}>Mục tiêu ngày</span>
-            <a style={{ float: "right" }}>Thay đổi</a>
+            <span className={classes['title']}>Mục tiêu ngày</span>
+            <a className={classes['change-goal']}>Thay đổi</a>
           </h2>
+          <div className={classes['streak-container']}>
+            <div className={classes['streak-circle-container']}>
+              <div className={classes['streak-circle']}>
+                <span>1</span>
+                <span className={classes['sub-text']}>ngày</span>
+              </div>
+            </div>
+            <div className={classes['streak-goal-container']}>
+              <div className={classes['main-text']}>10/10</div>
+              <div className={classes['sub-text']}>xp cần</div>
+              <div className={classes['main-text']}>23</div>
+              <div className={classes['sub-text']}>tiếng còn lại</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

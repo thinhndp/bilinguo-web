7364dd5998a768005a34e6ea2feac5ba507883f5
tligodsp <43437080+tligodsp@users.noreply.github.com>
2020-01-09 22:24:21 +0700

    Done Responsive Page Post

--- src/layout/pages/PagePost/PagePost.jsx
+++ src/layout/pages/PagePost/PagePost.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { GoTriangleUp, GoTriangleDown } from 'react-icons/go';
 import MyNavbar from '../../components/MyNavbar/MyNavbar';
-import { useParams } from 'react-router-dom';
 import TextareaAutosize from 'react-textarea-autosize';
 import { useHistory } from 'react-router-dom';
 import { IoMdSend, IoIosAttach, IoIosImages, IoMdCamera, IoMdChatboxes } from 'react-icons/io';
 import uuid from 'uuid';
+
+import { useMediaQuery } from 'react-responsive'
 
 import classes from './PagePost.module.scss';
 
@@ -20,6 +21,11 @@ const PagePost = (props) => {
   const history = useHistory();
   const posts = localStorage.getItem('posts') ? JSON.parse(localStorage.getItem('posts')) : mockPosts;
   // var posts = [];
+
+  //responsive
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+  const isMobile = useMediaQuery({ maxDeviceWidth: 768 });
 
   useEffect(() => {
     if (!localStorage.getItem('posts')) {
@@ -235,22 +241,28 @@ const PagePost = (props) => {
                 src={comment.author.profilePicture}
                 alt='comment author pic'
               />
-              <div className={classes['comment-bubble']}>
-                <div className={classes['comment-votes-section']}>
-                  <GoTriangleUp 
-                    className={
-                      classes['vote'] + ((isUpvotedByUser(comment.upvoters)) ? (' ' + classes['upvote-color']) : (''))
-                    }
-                    onClick={() => { voteComment('upvote', comment) }}
-                  />
-                  <div className={classes['vote-count']}>{comment.upvoteCount - comment.downvoteCount}</div>
-                  <GoTriangleDown
-                    className={
-                      classes['vote'] + ((isDownvotedByUser(comment.downvoters)) ? (' ' + classes['downvote-color']) : (''))
-                    }
-                    onClick={() => { voteComment('downvote', comment) }}
-                  />
-                </div>
+              <div className={
+                classes['comment-bubble']
+                + (isMobile ? (' ' + classes['pl-20'] + ' ' + classes['flex-dir-col']) : (''))
+              }>
+                {
+                  !isMobile &&
+                  <div className={classes['comment-votes-section']}>
+                    <GoTriangleUp 
+                      className={
+                        classes['vote'] + ((isUpvotedByUser(comment.upvoters)) ? (' ' + classes['upvote-color']) : (''))
+                      }
+                      onClick={() => { voteComment('upvote', comment) }}
+                    />
+                    <div className={classes['vote-count']}>{comment.upvoteCount - comment.downvoteCount}</div>
+                    <GoTriangleDown
+                      className={
+                        classes['vote'] + ((isDownvotedByUser(comment.downvoters)) ? (' ' + classes['downvote-color']) : (''))
+                      }
+                      onClick={() => { voteComment('downvote', comment) }}
+                    />
+                  </div>
+                }
                 <div className={classes['comment-detail']}>
                   {/* <div className={classes['comment-author-name']}>
                     {comment.author.displayName} <span className={classes['posted-time']}>{comment.postedTime} trước</span>
@@ -260,6 +272,27 @@ const PagePost = (props) => {
                   </div>
                   <div className={classes['content']}>{comment.content}</div>
                 </div>
+                {
+                  isMobile &&
+                  <div>
+                    <div className={classes['line-m-10']}></div>
+                    <div className={classes['comment-votes-section-row']}>
+                      <GoTriangleUp 
+                        className={
+                          classes['vote'] + ((isUpvotedByUser(comment.upvoters)) ? (' ' + classes['upvote-color']) : (''))
+                        }
+                        onClick={() => { voteComment('upvote', comment) }}
+                      />
+                      <div className={classes['vote-count']}>{comment.upvoteCount - comment.downvoteCount}</div>
+                      <GoTriangleDown
+                        className={
+                          classes['vote'] + ((isDownvotedByUser(comment.downvoters)) ? (' ' + classes['downvote-color']) : (''))
+                        }
+                        onClick={() => { voteComment('downvote', comment) }}
+                      />
+                    </div>
+                  </div>
+                }
               </div>
             </div>
           )) }
@@ -271,109 +304,176 @@ const PagePost = (props) => {
   return (
     <div>
       <MyNavbar />
-      <div className={classes['page-body']}>
+      <div className={
+        classes['page-body']
+        + ' ' + (isDesktop ? (classes['page-body-destop']) : (classes['page-body-not-destop']))
+      }>
         {
           post != null
           ? <div className={classes['post-and-stat-container']}>
-            <div className={classes['post-column']}>
-              <div className={classes['profile-pic-and-vote-column']}>
-                <img
-                  className={classes['profile-pic']} 
-                  src={post.author.profilePicture}
-                  alt='author pic'
-                />
-                <div className={classes['votes-section']}>
-                  <GoTriangleUp
-                    className={
-                      classes['vote'] + ((isUpvotedByUser(post.upvoters)) ? (' ' + classes['upvote-color']) : (''))
-                    }
-                    onClick={() => { votePost('upvote') }}
+            <div className={
+              classes['post-column']
+              + (isDesktop ? ('') : (' ' + classes['unset-shadow']))
+            }>
+              {
+                !isMobile &&
+                <div className={classes['profile-pic-and-vote-column']}>
+                  <img
+                    className={classes['profile-pic']} 
+                    src={post.author.profilePicture}
+                    alt='author pic'
                   />
-                  <div className={classes['vote-count']}>{post.upvoteCount - post.downvoteCount}</div>
-                  <GoTriangleDown
-                    className={
-                      classes['vote'] + ((isDownvotedByUser(post.downvoters)) ? (' ' + classes['downvote-color']) : (''))
-                    }
-                    onClick={() => { votePost('downvote') }}
-                  />
+                  <div className={classes['votes-section']}>
+                    <GoTriangleUp
+                      className={
+                        classes['vote'] + ((isUpvotedByUser(post.upvoters)) ? (' ' + classes['upvote-color']) : (''))
+                      }
+                      onClick={() => { votePost('upvote') }}
+                    />
+                    <div className={classes['vote-count']}>{post.upvoteCount - post.downvoteCount}</div>
+                    <GoTriangleDown
+                      className={
+                        classes['vote'] + ((isDownvotedByUser(post.downvoters)) ? (' ' + classes['downvote-color']) : (''))
+                      }
+                      onClick={() => { votePost('downvote') }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className={classes['post-detail-column']}>
+              }
+              <div className={
+                classes['post-detail-column']
+                + (isMobile ? (' ' + classes['pl-40']) : (''))
+              }> 
+                {
+                  isMobile &&
+                  <div className={classes['user-and-time-row']}>
+                    <img
+                      className={classes['profile-picture']}
+                      src={post.author.profilePicture}
+                      alt='profile-pic'
+                    />
+                    <div className={classes['username-and-time']}>
+                      <div className={classes['username']}>{post.author.displayName}</div>
+                      <div className={classes['subtext-2']}>{helper.getFormattedPostedTime(post.postedTime)}</div>
+                    </div>
+                  </div>
+                }
                 <div className={classes['post-title']}>{post.title}</div>
-                <div className={classes['author-and-posted-time-container']}>
-                  <span className={classes['subtext-1']}>{post.author.displayName}</span>
-                  <span className={classes['subtext-2']}>{' '} đã đăng {helper.getFormattedPostedTime(post.postedTime)}</span>
-                </div>
-                <div 
-                  className={classes['topic-tag']}
-                  style={{
-                    color: post.topic.backgroundColor,
-                  borderColor: post.topic.backgroundColor
-                    }}
-                ><span
-                  className={classes['dat-circle']}
-                  style={{
-                    backgroundColor: post.topic.backgroundColor
-                  }}
-                ></span>{post.topic.name}
-                </div>
+                {
+                  !isMobile &&
+                  <div>
+                    <div className={classes['author-and-posted-time-container']}>
+                      <span className={classes['subtext-1']}>{post.author.displayName}</span>
+                      <span className={classes['subtext-2']}>{' '} đã đăng {helper.getFormattedPostedTime(post.postedTime)}</span>
+                    </div>
+                    <div 
+                      className={classes['topic-tag']}
+                      style={{
+                        color: post.topic.backgroundColor,
+                      borderColor: post.topic.backgroundColor
+                        }}
+                    ><span
+                      className={classes['dat-circle']}
+                      style={{
+                        backgroundColor: post.topic.backgroundColor
+                      }}
+                    ></span>{post.topic.name}
+                    </div>
+                  </div>
+                }
                 <div className={classes['line']}></div>
                 <p className={classes['post-content']}>{post.content}</p>
+                {
+                  isMobile &&
+                  <div className={classes['votes-n-topic-row']}>
+                    <div className={classes['votes-container']}>
+                      <GoTriangleUp
+                        className={
+                          classes['vote'] + ((isUpvotedByUser(post.upvoters)) ? (' ' + classes['upvote-color']) : (''))
+                        }
+                        onClick={() => { votePost('upvote') }}
+                      />
+                      <div className={classes['vote-count']}>{post.upvoteCount - post.downvoteCount}</div>
+                      <GoTriangleDown
+                        className={
+                          classes['vote'] + ((isDownvotedByUser(post.downvoters)) ? (' ' + classes['downvote-color']) : (''))
+                        }
+                        onClick={() => { votePost('downvote') }}
+                      />
+                    </div>
+                    <div 
+                      className={classes['topic-tag'] + ' ' + classes['m-top-unset']}
+                      style={{
+                        color: post.topic.backgroundColor,
+                      borderColor: post.topic.backgroundColor
+                        }}
+                    ><span
+                      className={classes['dat-circle']}
+                      style={{
+                        backgroundColor: post.topic.backgroundColor
+                      }}
+                    ></span>{post.topic.name}
+                    </div>
+                  </div>
+                }
                 { renderCommentSection(comments) }
               </div>
             </div>
-            <div className={classes['stat-column']}>
-              <div className={classes['section-card']}>
-                <div className={classes['section-name']}>Bài viết liên quan</div>
-                <div className={classes['line']}></div>
-                <div className={classes['related-posts-section']}>
-                  { getRelatedPost(post).length > 0 
-                  ? getRelatedPost(post).map(post => (
-                    <div className={classes['related-post']}>
-                      <div
-                        className={classes['title']}
-                        onClick={handleRelatedPostClick.bind(this, post.id)}
-                      >{post.title}</div>
-                      <div className={classes['subtext']}>Đăng bởi 
-                        <span><img
-                          className={classes['profile-picture']}
-                          src={post.author.profilePicture}
-                          alt='pp'
-                        /></span> 
-                        {helper.getFormattedPostedTime(post.postedTime)}
+            {
+              isDesktop &&
+              <div className={classes['stat-column']}>
+                <div className={classes['section-card']}>
+                  <div className={classes['section-name']}>Bài viết liên quan</div>
+                  <div className={classes['line']}></div>
+                  <div className={classes['related-posts-section']}>
+                    { getRelatedPost(post).length > 0 
+                    ? getRelatedPost(post).map(post => (
+                      <div className={classes['related-post']}>
+                        <div
+                          className={classes['title']}
+                          onClick={handleRelatedPostClick.bind(this, post.id)}
+                        >{post.title}</div>
+                        <div className={classes['subtext']}>Đăng bởi 
+                          <span><img
+                            className={classes['profile-picture']}
+                            src={post.author.profilePicture}
+                            alt='pp'
+                          /></span> 
+                          {helper.getFormattedPostedTime(post.postedTime)}
+                        </div>
                       </div>
-                    </div>
-                  ))
-                  : <div style={{ fontSize: '16px', color: '#999' }}>Không tìm thấy bài viết liên quan</div>
-                  }
+                    ))
+                    : <div style={{ fontSize: '16px', color: '#999' }}>Không tìm thấy bài viết liên quan</div>
+                    }
+                  </div>
+                </div>
+                <div className={classes['section-card']}>
+                  <div className={classes['section-name']}>Thành viên năng nổ</div>
+                  <div className={classes['line']}></div>
+                  <div className={classes['top-users-section']}>
+                    { mockTopUsers.length > 0 
+                    ? mockTopUsers.map(topUser => (
+                      <div className={classes['top-user']}>
+                        <div className={classes['username-and-pp']}>
+                          <img
+                            className={classes['profile-picture']}
+                            src={topUser.user.profilePicture}
+                            alt='pp'
+                          />  
+                          <div className={classes['username']}>{topUser.user.displayName}</div>
+                        </div>
+                        <div className={classes['comment-count']}>
+                          <IoMdChatboxes className={classes['icon']}/>
+                          <div className={classes['count']}>{topUser.commentCount}</div>
+                        </div>
+                      </div>
+                    ))
+                    : <div style={{ fontSize: '16px', color: '#999' }}>Không tìm thấy thành viên năng nổ</div>
+                    }
+                  </div>
                 </div>
               </div>
-              <div className={classes['section-card']}>
-                <div className={classes['section-name']}>Thành viên năng nổ</div>
-                <div className={classes['line']}></div>
-                <div className={classes['top-users-section']}>
-                  { mockTopUsers.length > 0 
-                  ? mockTopUsers.map(topUser => (
-                    <div className={classes['top-user']}>
-                      <div className={classes['username-and-pp']}>
-                        <img
-                          className={classes['profile-picture']}
-                          src={topUser.user.profilePicture}
-                          alt='pp'
-                        />  
-                        <div className={classes['username']}>{topUser.user.displayName}</div>
-                      </div>
-                      <div className={classes['comment-count']}>
-                        <IoMdChatboxes className={classes['icon']}/>
-                        <div className={classes['count']}>{topUser.commentCount}</div>
-                      </div>
-                    </div>
-                  ))
-                  : <div style={{ fontSize: '16px', color: '#999' }}>Không tìm thấy thành viên năng nổ</div>
-                  }
-                </div>
-              </div>
-            </div>
+            }
           </div>
           : null
         }

6d1dd8742898c39f74c47a6add68932cc772808c
tligodsp <43437080+tligodsp@users.noreply.github.com>
2020-01-10 21:55:03 +0700

    Done Landing Page and Navbar

--- src/layout/components/MyNavbar/MyNavbar.jsx
+++ src/layout/components/MyNavbar/MyNavbar.jsx
@@ -1,20 +1,132 @@
-import React from 'react';
-import { Container, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Button, Dropdown, DropdownItem } from 'react-bootstrap';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import { FiList } from 'react-icons/fi';
+import { MdStore, MdFace, MdWeb, MdForum  } from 'react-icons/md';
+import { HamburgerArrow, HamburgerSpin } from 'react-animated-burgers';
 
 import classes from './MyNavbar.module.scss';
 
-function MyNavbar() {
+function MyNavbar(props) {
+  const [burgerIsActive, setBurgurIsActive] = useState(false);
+  const history = useHistory();
+  const location = useLocation();
+
+  const isDesktop = useMediaQuery({ minWidth: 1224 });
+
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <div
+      ref={ref}
+      onClick={e => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {children}
+    </div>
+  ));
+
+  const getCurrentActiveRoute = () => {
+    if (location.pathname.includes('learn')) {
+      return 'learn';
+    }
+    if (location.pathname.includes('forum') || location.pathname.includes('post') || location.pathname.includes('new-post')) {
+      return 'forum';
+    }
+    if (location.pathname.includes('shop')) {
+      return 'shop';
+    }
+    if (location.pathname.includes('tutors')) {
+      return 'tutors';
+    }
+    return 'nope';
+  }
+  
+  const toggleBurger = () => {
+    setBurgurIsActive(!burgerIsActive);
+    console.log(location);
+  }
+
   return (
     <div className={classes['navbar']}>
       <Container className={classes['navbar-inner-container']}>
         <div className={classes['navbar-brand']}>
           <a href="/">Bilinguo</a>
         </div>
-        <div className={classes['navbar-right']}>
-          <div className="mr-3">Ngôn ngữ hiển thị: Tiếng Việt</div>
-          <Button variant="secondary" className="mr-2">Đăng ký</Button>
-          <Button variant="success">Đăng nhập</Button>
-        </div>
+        {
+          isDesktop &&
+          <div className={classes['navbar-middle']}>
+            <div
+              className={
+                classes['nav-item']
+                + ((getCurrentActiveRoute() === 'learn') ? (' ' + classes['nav-item-active']) : (''))
+              }
+              onClick={() => { history.push('/learn') }}
+            >
+              <MdWeb className={classes['icon']}></MdWeb>
+              <div>Bài tập</div>
+            </div>
+            <div
+              className={
+                classes['nav-item']
+                + ((getCurrentActiveRoute() === 'forum') ? (' ' + classes['nav-item-active']) : (''))
+              }
+              onClick={() => { history.push('/forum') }}
+            >
+              <MdForum className={classes['icon']}></MdForum>
+              <div>Diễn đàn</div>
+            </div>
+            <div
+              className={
+                classes['nav-item']
+                + ((getCurrentActiveRoute() === 'shop') ? (' ' + classes['nav-item-active']) : (''))
+              }
+              onClick={() => { history.push('/shop') }}
+            >
+              <MdStore className={classes['icon']}></MdStore>
+              <div>Cửa hàng</div>
+            </div>
+            <div
+              className={
+                classes['nav-item']
+                + ((getCurrentActiveRoute() === 'tutors') ? (' ' + classes['nav-item-active']) : (''))
+              }
+              onClick={() => { history.push('/tutors') }}
+            >
+              <MdFace className={classes['icon']}></MdFace>
+              <div>Gia sư</div>
+            </div>
+          </div>
+        }
+        {
+          isDesktop &&
+          <div className={classes['navbar-right']}>
+            <Button variant="secondary" className="mr-2">Đăng ký</Button>
+            <Button variant="success">Đăng nhập</Button>
+          </div>
+        }
+        {
+          !isDesktop &&
+          <div>
+          <Dropdown>
+            <Dropdown.Toggle as={CustomToggle} variant="success" id="dropdown-basic">
+              {/* <FiList></FiList> */}
+              <HamburgerSpin isActive={burgerIsActive} toggleButton={toggleBurger} barColor="white" />
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu className={classes['dd-menu']}>
+              <Dropdown.Item className={classes['dd-text']} href="/learn">Bài tập</Dropdown.Item>
+              <Dropdown.Item className={classes['dd-text']} href="/forum">Diễn đàn</Dropdown.Item>
+              <Dropdown.Item className={classes['dd-text']} href="/shop">Cửa hàng</Dropdown.Item>
+              <Dropdown.Item className={classes['dd-text']} href="/tutors">Gia sư</Dropdown.Item>
+              <DropdownItem divider />
+              <Dropdown.Item className={classes['dd-text']} href="/">Đăng ký</Dropdown.Item>
+              <Dropdown.Item className={classes['dd-text']} href="/">Đăng nhập</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          </div>
+        }
       </Container>
     </div>
   );
--- src/layout/pages/PageHome/components/Showcase/Showcase.jsx
+++ src/layout/pages/PageHome/components/Showcase/Showcase.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
 
+import { useMediaQuery } from 'react-responsive';
+
 import classes from './Showcase.module.scss';
 
 function Home() {
   // const [toggler, setToggler] = useState(false);
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
   return (
     <div className={classes['showcase']}>
       <div className={classes['opacity-layer']} />
-      <Container className={classes['showcase-inner-container']}>
-        <Button size="lg" href="/learn">Bắt đầu</Button>
-      </Container>
+      <div className={classes['showcase-inner-container']}>
+        {
+          isDesktop &&
+          <div className={classes['globe-img']}>
+            <img
+              style={{
+                width: '540px',
+                height: '540px'
+              }}
+              src={require('../../../../../assets/globe1.png')}
+              alt='earth'
+            ></img>
+          </div>
+        }
+        <div className={classes['text-and-button']}>
+          <h1>Học Tiếng Anh Miễn Phí</h1>
+          <p>Một phương pháp học tiếng anh bằng hình thức giải trí, đồng thời trau dồi tất cả những kỹ năng.</p>
+          <Button
+            size="lg"
+            href="/learn"
+            className={classes['button']}
+          >Bắt đầu</Button>
+        </div>
+      </div>
     </div>
   )
 }
